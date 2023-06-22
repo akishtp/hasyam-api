91e8009255e139c149ef4f9544b911e2308bdd32
akishtp <makishtp@gmail.com>
2023-06-22 21:09:37 +0530

    submit a joke - not done yet. I just thought of something. that is also like an important part of the api. I should have implemented earlier itslef. This happened because I blatantly copied the design from another site

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      minHeight: {
+        100: "10rem",
+      },
+    },
   },
   plugins: [],
 };
