69ccd7c60596b437ba518567ac4d5593298856ec
akishtp <makishtp@gmail.com>
2023-06-23 14:07:07 +0530

    wtf was wrong last commit II

--- joke/notApprovedModel.ts
+++ joke/notApprovedModel.ts
@@ -1,7 +1,3 @@
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema;
-
 const notApprovedSchema = new Schema({
   _id: { type: Number, required: true, unique: true },
   joke: { type: String, required: true, unique: true },
