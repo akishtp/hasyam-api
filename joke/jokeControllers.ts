070fe4406bb3561f3884af9d9da19faa50c40565
akishtp <makishtp@gmail.com>
2023-06-25 11:54:39 +0530

    not Approved jokes??

--- joke/jokeControllers.ts
+++ joke/jokeControllers.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { Request, Response } from "express";
 const Joke = require("./jokeModel");
 
@@ -8,22 +9,39 @@ export const createJoke = async (req: Request, res: Response) => {
     res.status(200).json(da_joke);
   } catch (error: any) {
     res.status(400).json({ error: error.message });
-    return;
   }
 };
 
 export const getJoke = async (req: Request, res: Response) => {
   try {
     const count = await Joke.countDocuments({ approved: true }).exec();
-
     const randomIndex = Math.floor(Math.random() * count);
-
     const joke = await Joke.findOne({ approved: true })
       .skip(randomIndex)
       .exec();
     res.status(200).json(joke);
   } catch (error: any) {
     res.status(400).json({ error: error.message });
-    return;
   }
+};
+
+export const approveJoke = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const foundJoke = await Joke.findById(id);
+    foundJoke.approved = true;
+    await foundJoke.save();
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const notApproved = async (req: Request, res: Response) => {
+  Joke.find({ approved: false })
+    .then((jokes: any) => {
+      res.json(jokes);
+    })
+    .catch((error: any) => {
+      res.status(400).json({ error: error });
+    });
 };
--- joke/jokeRoutes.ts
+++ joke/jokeRoutes.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from "express";
-import { createJoke, getJoke } from "./jokeControllers";
-const Joke = require("./jokeModel");
+import {
+  approveJoke,
+  createJoke,
+  getJoke,
+  notApproved,
+} from "./jokeControllers";
 
 const router = express.Router();
 
 router.post("/", createJoke);
 router.get("/", getJoke);
+router.put("/:id", approveJoke);
+router.get("/notapproved", notApproved);
 
 module.exports = router;
