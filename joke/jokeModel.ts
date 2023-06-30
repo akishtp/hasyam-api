61f33b5a7dc97c649c399bb48bd1b3c7636943f2
akishtp <makishtp@gmail.com>
2023-06-30 19:26:08 +0530

    dark mode II

--- joke/jokeModel.ts
+++ joke/jokeModel.ts
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const joke = new Schema({
-  joke: { type: String, required: true, unique: true },
+  joke: { type: String, required: true },
   language: { type: String, required: true },
   approved: { type: Boolean, default: false },
 });
